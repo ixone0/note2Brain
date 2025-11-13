a71f8808448386e4663467eba9036aac133d2741
ixone0 <jasonboy025@gmail.com>
2025-11-13 18:59:23 +0700

    r

--- frontend/src/Register.jsx
+++ frontend/src/Register.jsx
@@ -39,7 +39,7 @@ export default function Register() {
     }
 
     try {
-      const res = await fetch("http://localhost:8000/register", {
+      const res = await fetch("https://note2brain-backend.onrender.com/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
